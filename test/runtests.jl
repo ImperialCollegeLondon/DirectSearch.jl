61d3e58180919e9ade466df3ae04b8c9a5fc1a5c
EdwardStables <edward.stables1198@gmail.com>
2020-05-24 18:01:06 +0100

    Re-enable tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,11 @@ using DirectSearch
 using Test
 
 include("./test_OrthoMADS.jl")
-#=
+
 include("./test_LTMADS.jl")
 include("./test_Cache.jl")
 include("./test_Search.jl")
 include("./test_Constraints.jl")
 include("./test_Core.jl")
-=#
-#Disabled for now as LTMADS can fail sometimes
-#include("./solve_tests.jl")
+include("./solve_tests.jl")
 
