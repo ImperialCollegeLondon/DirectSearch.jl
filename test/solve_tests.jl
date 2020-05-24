af310a2a025ee7713a7b44d4be53cc9b2555043b
EdwardStables <edward.stables1198@gmail.com>
2020-05-24 23:19:11 +0100

    replaced LTMADS with OrthoMADS

--- test/solve_tests.jl
+++ test/solve_tests.jl
@@ -1,7 +1,7 @@
 @testset "Test Problems" begin
     @testset "Unconstrained" begin 
         @testset "camel6" begin
-            p = DSProblem(2; objective=DS.camel6, initial_point=[5.0, 5.0])
+            p = DSProblem(2; poll=OrthoMADS(2), objective=DS.camel6, initial_point=[5.0, 5.0])
             Optimize!(p)
             @test isapprox(abs(p.x[1]), 0.0898, atol=1e-4)
             @test isapprox(abs(p.x[2]), 0.7126, atol=1e-4) 
@@ -9,7 +9,7 @@
             @test p.iteration < 120
         end
         @testset "audet_dennis_benchmark_1" begin
-            p = DSProblem(2; objective=DS.bm_1, initial_point=[-2.1, 1.7])
+            p = DSProblem(2; poll=OrthoMADS(2), objective=DS.bm_1, initial_point=[-2.1, 1.7])
             Optimize!(p)
             @test isapprox(p.x[1], 0, atol=1e-8)
             @test isapprox(p.x[2], 0, atol=1e-8)
@@ -19,7 +19,7 @@
     end
     @testset "Extreme Constraints" begin
         @testset "camel6" begin
-            p = DSProblem(2; objective=DS.camel6, initial_point=[5.0, 5.0])
+            p = DSProblem(2; poll=OrthoMADS(2), objective=DS.camel6, initial_point=[5.0, 5.0])
             AddExtremeConstraint(p, x-> x[1] > 0)
             Optimize!(p)
             @test isapprox(p.x[1], 0.0898, atol=1e-4)
@@ -27,7 +27,7 @@
             @test isapprox(p.x_cost, -1.0316, atol=1e-4)
             @test p.iteration < 120
 
-            p = DSProblem(2; objective=DS.camel6, initial_point=[-5.0, -5.0])
+            p = DSProblem(2; poll=OrthoMADS(2), objective=DS.camel6, initial_point=[-5.0, -5.0])
             AddExtremeConstraint(p, x->x[1] < 0)
             Optimize!(p)
             @test isapprox(p.x[1], -0.0898, atol=1e-4)
@@ -37,8 +37,9 @@
         end
     end
     @testset "Progressive Constraints" begin
+        #Progressive barrier constraints struggle to converge with LTMADS, therefore use OrthoMADS
         @testset "camel6" begin
-            p = DSProblem(2; objective=DS.camel6, initial_point=[5.0, 5.0])
+            p = DSProblem(2; poll=OrthoMADS(2), objective=DS.camel6, initial_point=[5.0, 5.0])
             AddProgressiveConstraint(p, x -> x[1])
             Optimize!(p)
             @test isapprox(p.x[1], -0.0898, atol=1e-4)
@@ -46,7 +47,7 @@
             @test isapprox(p.x_cost, -1.0316, atol=1e-4)
             @test p.iteration < 200
 
-            p = DSProblem(2; objective=DS.camel6, initial_point=[-5.0, -5.0])
+            p = DSProblem(2; poll=OrthoMADS(2), objective=DS.camel6, initial_point=[-5.0, -5.0])
             AddProgressiveConstraint(p, x->-x[1])
             Optimize!(p)
             @test isapprox(p.x[1], 0.0898, atol=1e-4)
@@ -54,21 +55,5 @@
             @test isapprox(p.x_cost, -1.0316, atol=1e-4)
             @test p.iteration < 200
         end
-        
-        #@testset "audet_dennis_benchmark_3" begin
-        #    #TODO update the constraints on result accuracy
-        #    #as the package gives better results.
-        #    N = 5
-        #    p = DSProblem(5; objective=DS.bm_3, initial_point=zeros(5))
-        #    AddProgressiveConstraint(p, DS.bm_3_con)
-        #    Optimize!(p)
-        #    @show p.x 
-        #    @show p.i 
-        #    for x in p.x
-        #        @test isapprox(x, -√3, atol=1)
-        #    end
-        #    @test isapprox(p.x_cost, N * -√3, atol=0.5)
-        #    @test p.iteration <= 1000
-        #end
     end
 end
