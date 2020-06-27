a3b0fde8837e64b07099ceda7e53427f776e2590
EdwardStables <edward.stables1198@gmail.com>
2020-06-27 14:24:03 +0100

    Syntax error fixes on test functions

--- src/test_utils.jl
+++ src/test_utils.jl
@@ -5,13 +5,13 @@ const DS = DirectSearch
 #= Following functions sourced from https://www.sfu.ca/~ssurjano/optimization.html =#
 #Valley
 #soln: -1.0316 @ [±0.0898, ∓0.7126]
-camel6(x)=(4-2.1x[1]^2 + x[1]^4/3)x[1]^2 + x[1]x[2] + (-4 + 4x[2]^2)x[2]^2
+camel6(x)=(4-2.1x[1]^2.0 + x[1]^4/3)x[1]^2.0 + x[1]x[2] + (-4 + 4x[2]^2.0)x[2]^2.0
 #soln: 0 @ [1,...,1]
 rosenbrock(x;a=1,b=100,d=length(x)) = sum([b*(x[i+1] - x[i]^2)^2 + (x[i]-a)^2 for i=1:d-1])
 
 #Local Minima
 ackley(x;a=20,b=0.2,c=2pi,d=length(x)) = -a * exp(-b*√(sum(x.^2)/d)) - exp(sum(cos.(c.*x))/d) + a + exp(1)
-eggholde(rx) = -(x[2]+47)sin(√abs(x[2] + x[1]/2 + 47)) - x[1]sin(√abs(x[1]-x[2]+47))
+eggholder(x) = -(x[2]+47)sin(√abs(x[2] + x[1]/2 + 47)) - x[1]sin(√abs(x[1]-x[2]+47))
 
 #Example Benchmarks from papers
 #Audet & Dennis 2006 benchmark one, take x₀=[-2.1,1.7]
