ec51999fe5cd11b07b67e27514deeba864c26e65
Ian McInerney <ian.s.mcinerney@ieee.org>
2020-07-16 18:58:08 +0100

    Fix some docs parts.

--- docs/make.jl
+++ docs/make.jl
@@ -6,24 +6,24 @@ makedocs(;
     pages=[
         "Home" => "index.md",
         "Manual" => [
-            "Usage" => "man/usage.md", 
-            "Reporting" => "man/reporting.md", 
-            "Adding a Search Step" => "man/addsearch.md", 
-            "Adding a Poll Step" => "man/addpoll.md", 
-            "Constraints" => "man/constraints.md", 
-            "Cache" => "man/cache.md", 
+            "Usage" => "man/usage.md",
+            "Reporting" => "man/reporting.md",
+            "Adding a Search Step" => "man/addsearch.md",
+            "Adding a Poll Step" => "man/addpoll.md",
+            "Constraints" => "man/constraints.md",
+            "Cache" => "man/cache.md",
            ],
         "Reference" => [
             "Public API" => "ref/public.md",
             "Internal" => "ref/internal.md",
            ],
     ],
-    repo="https://github.com/EdwardStables/DirectSearch.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/ImperialCollegeLondon/DirectSearch.jl/blob/{commit}{path}#L{line}",
     sitename="DirectSearch.jl",
     authors="EdwardStables <edward.stables1198@gmail.com>",
     assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/EdwardStables/DirectSearch.jl",
+    repo="github.com/ImperialCollegeLondon/DirectSearch.jl",
 )
